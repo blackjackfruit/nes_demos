21f5e1a2dafe1677aa91edee199d7e7b1d0096c8
iury bessa <ibessa@ymail.com>
2016-10-27 21:49:48 -0400

    added missing plotter_game.asm from previous commit

---
+++ plotter_game.asm
@@ -0,0 +1,378 @@
+  .inesprg 1   ; 1x 16KB PRG code
+  .ineschr 1   ; 1x  8KB CHR data
+  .inesmap 0   ; mapper 0 = NROM, no bank swapping
+  .inesmir 1   ; background mirroring
+
+; Constants
+PPU_CTRL    = $2000
+PPU_MASK    = $2001
+PPU_STATUS  = $2002
+PPU_SCROLL  = $2005
+PPU_ADDRESS = $2006
+PPU_DATA    = $2007
+
+; global variables to be used as parameters for functions
+  .rsset $0000
+variable1 .rs 1
+variable2 .rs 1
+variable3 .rs 1
+variable4 .rs 1
+variable5 .rs 2
+
+sprite_x_pos .rs 1
+sprite_y_pos .rs 1
+was_button_released .rs 1
+
+frame_counter .rs 1
+seconds_in_ones_position .rs 1
+seconds_in_tens_position .rs 1
+seconds_in_hundredth_position .rs 1
+
+sprite_counter .rs 1 ; offset for the number of sprites
+
+  .bank 0
+  .org $C000
+
+; Starting point of the program, ie kinda like main()
+START:
+  SEI           ; Set interrupt disabled
+  CLD           ; Clear Decimal Mode even though the NES doesn't support it
+  LDX #$40
+  STX $4017
+  LDX #$FF
+  TXS           ; Sets the stack to FF. The stack address will start at $01FF
+                ; and grow down to $0100 when pushes are done onto the stack.
+  INX           ; Since X is FF, incrementing X by one will roll over X to 0
+  STX PPU_CTRL  ; Set PPU_CTRL to 0
+  STX PPU_MASK  ; Set PPU_MASK to 0
+  STX $4010
+  LDA #$01
+  STA was_button_released
+  LDA #$00
+  STA sprite_counter
+  JSR vBlank    ; wait for Vertical Blank
+
+; Set memory values between $0100 through $0700 to 0.
+; X is set to 0 in START: because after transferring FF to the stack, X is
+; incremented by one which rolls the byte to 0
+; The addresses between $0200 - $02FF are set to #$FE
+MEMCLEAR:
+  LDA #$00
+  STA $0100, x
+  STA $0300, x
+  STA $0400, x
+  STA $0500, x
+  STA $0600, x
+  STA $0700, x
+  LDA #$FE
+  STA $0200, x
+  INX
+  BNE MEMCLEAR
+
+  JSR vBlank ; wait for Vertical Blank again
+
+LoadPalettes:
+  ; It is necessary to call PPU_ADDRESS twice so we can pass the high low
+  ; bytes for the address. In this case the address where we are sending
+  ; data is $3F00 for palette data
+  LDA PPU_STATUS        ; read PPU status to reset the high/low latch
+  LDA #$3F
+  STA PPU_ADDRESS       ; write the high byte of $3F00 address
+  LDA #$00
+  STA PPU_ADDRESS       ; write the low byte of $3F00 address
+  LDX #$00              ; start out at 0
+LoadPalettesLoop:
+  LDA PALETTES, x       ; load data from address (palette + the value in x)
+                        ; 1st time through loop it will load palette+0
+                        ; 2nd time through loop it will load palette+1
+                        ; 3rd time through loop it will load palette+2
+                        ; etc
+  STA PPU_DATA          ; write to PPU
+  INX                   ; X = X + 1
+  CPX #$20              ;
+  BNE LoadPalettesLoop  ;
+
+  ; Load 8 sprites worth of data
+  ; The first sprite data is between $0200-$0203, then the second sprite is
+  ; $0204-$0208, etc. Every sprite is 4 bytes long.
+  ; The byte order is...
+  ; X (Vertical positioning), Tile, Sprite Palette, Y (Horizontal positioning)
+  LDA #$77
+  STA sprite_x_pos
+  STA variable1
+  LDA #$78
+  STA sprite_y_pos
+  STA variable4
+  LDA #$04
+  STA variable2
+  LDA #$03
+  STA variable3
+  JSR LoadSprite
+
+  LDA #$6F
+  STA variable1
+  LDA #$78
+  STA variable4
+  LDA #$03
+  STA variable2
+  LDA #$02
+  STA variable3
+  JSR LoadSprite
+
+  LDA #$67
+  STA variable1
+  LDA #$70
+  STA variable4
+  LDA #$05
+  STA variable2
+  LDA #$02
+  STA variable3
+  JSR LoadSprite
+
+  LDA #$21
+  STA variable1
+  LDA #$AE
+  STA variable2
+LoadBackground:
+  LDA PPU_STATUS
+  LDA variable1
+  STA PPU_ADDRESS
+  LDA variable2
+  STA PPU_ADDRESS
+  LDX #$00
+LoadBackgroundLoop:
+  LDA #$01
+  STA PPU_DATA
+  STA PPU_DATA
+  STA PPU_DATA
+  STA PPU_DATA
+  LDA PPU_STATUS
+  LDA variable1
+  STA PPU_ADDRESS
+  LDA variable2
+  CLC
+  ADC #$20
+  STA variable2
+  STA PPU_ADDRESS
+  INX
+  CPX #$03
+  BNE LoadBackgroundLoop
+
+LoadAttributes:
+  LDA PPU_STATUS
+  LDA #$23
+  STA PPU_ADDRESS
+  LDA #$C0
+  STA PPU_ADDRESS
+  LDX #$00
+LoadAttributesLoop:
+  LDA #$00
+  STA PPU_DATA
+  INX
+  CPX #$FF
+  BNE LoadAttributesLoop
+
+  LDA #%10001000   ; enable NMI, sprites from Pattern Table 0, background from Pattern Table 1
+  STA PPU_CTRL
+
+  LDA #%10011110   ; enable sprites, enable background, no clipping on left side
+  STA PPU_MASK
+
+INFINITY:
+  JMP INFINITY
+
+NMI:
+  ; Save the state of A, X, Y
+  PHA
+  TXA
+  PHA
+  TYA
+  PHA
+
+
+START_NMI:
+  STA frame_counter
+  LDA #$00
+  STA $2003       ; set the low byte (00) of the RAM address
+  LDA #$02
+  STA $4014       ; set the high byte (02) of the RAM address, start the transfer
+
+  ; Read the input from the user to change the sprite palette(s)
+  JSR ReadControllerInput
+
+  JSR Update_Counter
+
+  LDA #%10010000   ; enable NMI, sprites from Pattern Table 0, background from Pattern Table 1
+  STA PPU_CTRL
+  LDA #%00011110   ; enable sprites, enable background, no clipping on left side
+  STA PPU_MASK
+  LDA #$00        ;;tell the ppu there is no background scrolling
+  STA PPU_SCROLL
+  STA PPU_SCROLL
+
+;; Res
+  RTI
+
+; Function to display text on screen
+; Parameters
+;   * variable 1 & 2 - First and second bytes are the screen position.
+;     The addresses available are from $2000 to $2400
+;   * variable 3 - Length of the string
+;   * varaible 4 - NOT USED
+;   * variable 5 - The starting address for the string. If the text label
+;     is STRING_LABEL, you can use the LDA #LOW(STRING_LABEL) and
+;     LDA #HIGH(STRING_LABEL) to store the high/low byte address of a label
+Display_Text:
+  LDA PPU_STATUS
+  LDA variable1
+  STA PPU_ADDRESS
+  LDA variable2
+  STA PPU_ADDRESS
+  LDY #$00
+Display_Text_Loop:
+  LDA [variable5], y
+  STA PPU_DATA
+  INY
+  CPY variable3
+  BNE Display_Text_Loop
+  RTS
+
+LoadSprite:
+  LDX sprite_counter
+  LDA variable1
+  STA $0200, x
+  LDA variable4
+  STA $0203, x
+  LDA variable2
+  STA $0201, x
+  LDA variable3
+  STA $0202, x
+  LDA sprite_counter
+  CLC
+  ADC #$04
+  STA sprite_counter
+  RTS
+
+Update_Counter:
+  ;026F, 0270
+  LDA PPU_STATUS
+  LDA #$20
+  STA PPU_ADDRESS
+  LDA #$62
+  STA PPU_ADDRESS
+  LDA #$39
+  STA PPU_DATA
+  STA PPU_DATA
+  STA PPU_DATA
+  STA PPU_DATA
+
+  RTS
+
+; Check player one input
+ReadControllerInput:
+  LDA #$01
+  STA $4016
+  LDA #$00
+  STA $4016
+
+  LDX was_button_released
+  CPX #$00
+  BEQ CHECK_IF_NO_BUTTONS_ARE_PRESSED
+
+  LDA $4016
+  LDA $4016
+  LDA $4016
+  LDA $4016
+
+CONTINUE_READING_INPUT_UP:
+  LDA $4016
+  AND #$01
+  BEQ CONTINUE_READING_INPUT_DOWN
+  LDA sprite_x_pos
+  CLC
+  SBC #07
+  STA sprite_x_pos
+  STA $0200
+  LDA #$00
+  STA was_button_released
+  JMP FINISHED_READING_INPUT
+
+CONTINUE_READING_INPUT_DOWN:
+  LDA $4016
+  AND #$01
+  BEQ CONTINUE_READING_INPUT_LEFT
+  LDA sprite_x_pos
+  CLC
+  ADC #08
+  STA sprite_x_pos
+  STA $0200
+  LDA #$00
+  STA was_button_released
+  JMP FINISHED_READING_INPUT
+
+CONTINUE_READING_INPUT_LEFT:
+  LDA $4016
+  AND #$01
+  BEQ CONTINUE_READING_INPUT_RIGHT
+  LDA sprite_y_pos
+  CLC
+  SBC #07
+  STA sprite_y_pos
+  STA $0203
+  LDA #$00
+  STA was_button_released
+  JMP FINISHED_READING_INPUT
+
+CONTINUE_READING_INPUT_RIGHT:
+  LDA $4016
+  AND #$01
+  BEQ FINISHED_READING_INPUT
+  LDA sprite_y_pos
+  CLC
+  ADC #08
+  STA sprite_y_pos
+  STA $0203
+  LDA #$00
+  STA was_button_released
+  JMP FINISHED_READING_INPUT
+
+FINISHED_READING_INPUT:
+  RTS
+CHECK_IF_NO_BUTTONS_ARE_PRESSED:
+  LDA $4016
+  AND #$01
+  BNE INPUT_CHECK_FINISHED_WITH_KEY_STILL_PRESSED
+  INX
+  CPX #$08
+  BNE CHECK_IF_NO_BUTTONS_ARE_PRESSED
+  LDA #$01
+  STA was_button_released
+INPUT_CHECK_FINISHED_WITH_KEY_STILL_PRESSED:
+  RTS
+
+  .bank 1
+  .org $E000
+PALETTES:
+  ; background palette
+  .db $0F,$20,$10,$00,  $0F,$20,$29,$0C,  $22,$30,$21,$0F,  $22,$27,$17,$0F
+  ; sprite palette
+  .db $0D,$0F,$2B,$0F,  $3D,$16,$05,$27,  $0F,$20,$06,$17,  $0F,$20,$29,$0C
+
+SPRITE:
+  ; X (Vertical positioning), Tile, Sprite Palette, Y (Horizontal positioning)
+  .db 119, $02, $01, 120  ; User controlled sprite
+
+vBlank:
+  BIT PPU_STATUS
+  BPL vBlank
+  RTS
+
+  ; It is necessary to specify where the labels for where NMI START can be found
+  .org $FFFA
+  .dw NMI
+  .dw START
+  .dw 0
+
+  .bank 2
+  .org $0000
+  .incbin "demo.chr"
